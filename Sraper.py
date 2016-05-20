28b18688c17acc1f8a0ddf7c6f14a586d98476f6
TDreckman <timdreckman@gmail.com>
2016-05-20 13:38:45 -0600

    Update #1 Web scrapper works to pull Title, day, month, year

--- Sraper.py
+++ Sraper.py
@@ -1,11 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
-#atom testing
 
-#testing from Pycharm
-#is this working?
-#really?
-#is this called pycharm now?
+url = "http://smithstix.com/music"
 
-#testing after change
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content)
+
+divs = soup.find_all("div")
+
+for div in divs:
+    print(div.text)
+
+#eventColumns = soup.find_all("div", {"class": "events-row"})
+g_data = soup.find_all("div", {"class": "events-list-container"})
+
+
+for item in g_data:
+        title = item.contents[1].find_all("div", {"class": "event-title"})[0].text
+        titleEncoded = title.encode("ascii")
+        day = item.contents[1].find_all("div", {"class": "day-container"})[0].text
+        dayEncoded = day.encode("ascii")
+        month = item.contents[1].find_all("div", {"class": "month-container"})[0].text
+        monthEncoded = month.encode("ascii")
+        year = item.contents[1].find_all("div", {"class": "year-container"})[0].text
+        yearEncoded = year.encode("ascii")
+
+print(titleEncoded, dayEncoded.strip(), monthEncoded.strip(), yearEncoded.strip())
+        #print (dayEncoded.strip())
+        #print (monthEncoded.strip())
+        #print (yearEncoded.strip())
